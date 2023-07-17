65bf5caf1953a16bb5b230ec4b33ea21423487d2
bculo <culobozo@gmail.com>
2023-07-17 15:28:25 +0200

    Inital web app for hotchocolate graphql server added

---
+++ HotchocolateEndpoint/HotchocolateEndpoint/Models/Book.cs
@@ -0,0 +1,12 @@
+namespace HotchocolateEndpoint.Models;
+
+public class Book
+{
+    public string Title { get; set; }
+    public Author Author { get; set; }
+}
+
+public class Author
+{
+    public string Name { get; set; }
+}
---
+++ HotchocolateEndpoint/HotchocolateEndpoint/Queries/Query.cs
@@ -0,0 +1,16 @@
+using HotchocolateEndpoint.Models;
+
+namespace HotchocolateEndpoint.Queries;
+
+public class Query
+{
+    public Book GetBook() =>
+        new Book
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+}
